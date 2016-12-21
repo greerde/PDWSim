a79a39cf484fb932f634b502b04504a9c5e3c831
Derek Greer <greerde@gmail.com>
2016-12-21 05:51:29 -0500

    Update class and freq values

--- Program.cs
+++ Program.cs
@@ -22,7 +22,6 @@ namespace PDWSIM
 			IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 15000); //send datagram to localhost on port 15000
 
 			PDW pdw = new PDW();
-			pdw.dt = DateTime.Now;
 			var d = pdw.Serialize();
 
 			// Set duration of PDW playback here
@@ -50,18 +49,18 @@ namespace PDWSIM
 					if (i % 150 == 0)
 					{
 						// 4500.5
-						d[23] = 0x45; d[22] = 0x8c; d[21] = 0xa4; d[20] = 0x00;
+						d[19] = 0x45; d[18] = 0x8c; d[17] = 0xa4; d[16] = 0x00;
 					}
 					// Every 1501th Freq is 5442
-					else if (i % 1501 == 0)
+					else if (i % 15001 == 0)
 					{
 						//5442
-						d[23] = 0x45; d[22] = 0xaa; d[21] = 0x10; d[20] = 0x00;
+						d[19] = 0x45; d[18] = 0xaa; d[17] = 0x10; d[16] = 0x00;
 					}
 					// Set all other Freqs = 0
 					else
 					{
-						d[23] = 0; d[22] = 0; d[21] = 0; d[20] = 0;
+						d[19] = 0; d[18] = 0; d[17] = 0; d[16] = 0;
 					}
 
 					// Send datagram
@@ -79,25 +78,20 @@ namespace PDWSIM
 
 	public class PDW
 	{
-		public int Header = 0; // using 32 bit int as header placeholder
-						//REPLACED: HeaderFlags recordHeader; //!< Record header
-		
-		public DateTime dt;
-		public long dtNanoSec;
-		//REPLACED: HighResolutionTime toa; //!< UTC seconds with picosecond precision
-
-		public float startFrequencyMHz = 0; //!< Start frequency in MHz (TOA)
-		public float stopFrequencyMHz = 0; //!< Stop frequency in MHz (TOA + PW)
+		public int counter = 0; // rolling counter 
 		public int receiverID = 1; //!< ID of the receiver that generated the PDW
+		
+		public long toa;
+
+		public float startFrequencyMHz = 1000; //!< Start frequency in MHz (TOA)
+		public float stopFrequencyMHz = 1000; //!< Stop frequency in MHz (TOA + PW)
 		public double pulseWidth = 0.0005; //!< Pulse width in seconds
 		public float pulseAmplitude = -5.4F; //! Pulse amplitude in dBm (verify not dBW)
 		public float snr = 10; //!< Signal to noise ratio in dB
 		public float phase = 0; //!< Phase in radians
 		public float phaseReferenceTime = 0; //!< Offset from TOA of phase measurement in seconds
-		public short errorFlags = 0;  // placeholder for error flags
-							   // REPLACED: ErrorFlags errorFlags; //!< Error conditions that occured during generation of this PDW
-		public short modulationFlags = 0;
-		//REPLACED: ModulationFlags modulationFlags; //!< Modulation types detected during generation of this PDW
+		public int errorFlags = 0;  // placeholder for error flags
+		public int modulationFlags = 0;
 		public float modulationParameters = 0; //!< in Hz or Hz/s as defined by EMD platinum
 		public float angleOfArrival = 0; //!< in radians
 		public float riseTime = 0; //!< Rise time in seconds
@@ -112,12 +106,11 @@ namespace PDWSIM
 		{
 			using (BinaryWriter writer = new BinaryWriter(m))
 			{
-				writer.Write(Header);
-				writer.Write(dt.ToBinary());
-				writer.Write(dtNanoSec);
+				writer.Write(counter);
+				writer.Write(receiverID);
+				writer.Write(toa);
 				writer.Write(startFrequencyMHz);
 				writer.Write(stopFrequencyMHz);
-				writer.Write(receiverID);
 				writer.Write(pulseWidth);
 				writer.Write(pulseAmplitude);
 				writer.Write(snr);
